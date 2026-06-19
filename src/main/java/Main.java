3391fbf695d09985936ca62e1f1df670c03a21a9
Mudit <mudit.25bcs10501@sst.scaler.com>
2026-06-19 11:37:53 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        while (true) {
+            System.out.print("$ ");
+ 
+            if (!scanner.hasNextLine()) {
+                break;
+            }
+ 
+            String input = scanner.nextLine();
+ 
+            if (input.isEmpty()) {
+                continue;
+            }
+ 
+            // For now, every command is treated as invalid.
+            System.out.println(input + ": command not found");
+        }
     }
 }
